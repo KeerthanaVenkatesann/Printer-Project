20824e0c36c47fd0282a89359bd837d573190841
Keerthana Venkatesan <135305964+KeerthanaVenkatesann@users.noreply.github.com>
2024-05-30 17:32:07 +0530

    start from today

---
+++ src/Component/Layout/Layout.jsx
@@ -0,0 +1,9 @@
+import React from 'react'
+
+export default function Layout() {
+  return (
+    <div className='d-flex justify-content-center align-items-center vh-100'>
+    <h1>  Helo guys,Welcome to our Project!😃</h1>
+    </div>
+  )
+}
