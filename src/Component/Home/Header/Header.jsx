4e8af59992f87ec0b626cbd038cc1a5345a5f59f
rajapriyaraja <rajapriyaraja14@gmail.com>
2024-05-31 21:03:16 +0530

    Header,Loader  Completed

--- src/Component/Home/Header/Header.jsx
+++ src/Component/Home/Header/Header.jsx
@@ -1,9 +1,24 @@
 import React from 'react'
+import { Navbar, Nav, NavDropdown, Container } from 'react-bootstrap';
 
 export default function Header() {
   return (
     <div>
-      Header
+    <Navbar bg="light" expand="lg">
+      <Container>
+        <Navbar.Brand href="#home">BrandName</Navbar.Brand>
+        <Navbar.Toggle aria-controls="basic-navbar-nav" />
+        <Navbar.Collapse id="basic-navbar-nav">
+          <Nav className="me-auto">
+            <Nav.Link href="#home">Home</Nav.Link>
+            <Nav.Link href="#service">Service</Nav.Link>
+            <Nav.Link href="#customized-prints">Customized Prints</Nav.Link>
+            <Nav.Link href="#about-us">About Us</Nav.Link>
+            <Nav.Link href="#contact">Contact</Nav.Link>
+          </Nav>
+        </Navbar.Collapse>
+      </Container>
+    </Navbar>
     </div>
   )
 }
