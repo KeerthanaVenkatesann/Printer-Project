01ed2916bd7c36843d15e706955681eb65443b0e
Keerthana Venkatesan <135305964+KeerthanaVenkatesann@users.noreply.github.com>
2024-07-05 22:39:33 +0530

    progress

--- src/Component/Home/GetStarted/GetStarted.jsx
+++ src/Component/Home/GetStarted/GetStarted.jsx
@@ -2,15 +2,16 @@ import React from "react";
 import "./GetStarted.css";
 import Whatsapp from "../../Whatsapp/Whatsapp";
 import ContentBoxTwo from "../ContentBoxOne/ContentBoxTwo";
+import CarouselTwo from "../HomePage/Carousel/CarouselTwo";
 
 export default function GetStarted() {
   return (
     <>
       <div className="top-container w-100 p-5">
         <div className=" d-flex justify-content-center   align-items-center mt-3  content-font ">
-          <div className="w-75 d-flex justify-content-around bg-given rounded">
+          <div className="w-75 d-flex justify-content-around bg-given rounded p-3">
             <div className="text-start ">
-              <h4 className="colors-get">Get In</h4>
+              <h5 className="colors-get">Get In</h5>
               <h1>Touch With Us</h1>
             </div>
             <div className="whatsapp-button  d-flex justify-content-center   align-items-center  ">
@@ -20,7 +21,7 @@ export default function GetStarted() {
           </div>
         </div>
       </div>
-      <ContentBoxTwo />
+  <CarouselTwo />
     </>
   );
 }
--- src/Component/Home/HomePage/Gallery/Gallery.jsx
+++ src/Component/Home/HomePage/Gallery/Gallery.jsx
@@ -2,24 +2,119 @@
 import React from "react";
 import "./Gallery.css";
 import Footer from "../../Footer/Footer";
-import images from "./ImagesContainer";  // Adjust the path accordingly
+import images from "./ImagesContainer"; // Adjust the path accordingly
 
 export default function Gallery() {
   return (
-    <div>
-      <div className="container-fluid p-5">
-        <div className="gallery p-5">
-          {images.map((image, index) => (
-            <div className="gallery-item" key={index}>
-              <img
-                className="gallery-image"
-                src={image.src}
-                alt={image.alt}
-              />
+    <div className="mt-5 pt-5">
+      <div className="d-flex justify-content-center mt-5">
+        <div>
+          <div className="album">
+            <div className="responsive-container-block bg">
+              <div className="responsive-container-block img-cont">
+                <img
+                  className="img"
+                  src="/Assests/Gallery/IMG_20240625_111409.jpg"
+                />
+                <img
+                  className="img"
+                  src="/Assests/Gallery/IMG_20240625_184800.jpg"
+                />
+                <img
+                  className="img img-last"
+                  src="/Assests/Gallery/IMG-20230323-WA0011.jpg"
+                />
+                <img
+                  className="img img-last"
+                  src="/Assests/Gallery/3X3 FOAM.jpg"
+                />
+                <img
+                  className="img img-last"
+                  src="/Assests/Gallery/bhat villa.jpg"
+                />
+              </div>
+              <div className="responsive-container-block img-cont">
+                <img
+                  className="img img-big"
+                  src="/Assests/Gallery/IMG_20240626_230016.jpg"
+                />
+                <img
+                  className="img img-big img-last"
+                  src="/Assests/Gallery/DSC_4816-removebg-preview (1).png"
+                />
+                <img
+                  className="img img-big img-last"
+                  src="/Assests/Gallery/Coffeeco.jpg"
+                />
+                <img
+                  className="img img-big"
+                  src="/Assests/Gallery/beauty salon.jpg"
+                />
+              </div>
+              <div className="responsive-container-block img-cont">
+                <img className="img" src="/Assests/Gallery/menu card.jpg" />
+                <img className="img" src="/Assests/Gallery/FLAYER.jpg" />
+                <img className="img" src="/Assests/Gallery/LETTER HEAD.jpg" />
+                <img className="img" src="/Assests/Gallery/bride to be.jpg" />
+              </div>
             </div>
-          ))}
+            {/* </div>
+<div className="album"> */}
+            <div className="responsive-container-block bg">
+              <div className="responsive-container-block img-cont">
+                <img className="img" src="/Assests/Gallery/neon board.jpg" />
+                <img
+                  className="img"
+                  src="/Assests/Gallery/Untitled design_20240615_185024_0000.png"
+                />
+                <img
+                  className="img img-last"
+                  src="/Assests/Gallery/10 x 10 MS 1.jpg"
+                />
+                 <img
+                  className="img img-last"
+                  src="/Assests/Gallery/mrg.jpg"
+                />
+                 <img
+                  className="img img-last"
+                  src="/Assests/Gallery/open bar.jpg"
+                />
+              </div>
+              <div className="responsive-container-block img-cont">
+            
+                <img
+                  className="img img-big img-last"
+                  src="/Assests/Gallery/STANDY.jpg"
+                />
+
+<img
+                  className="img img-big img-last"
+                  src="/Assests/Gallery/white pebbles.jpg"
+                />
+
+
+<img
+                  className="img img-big img-last"
+                  src="/Assests/Gallery/imagee.jpg"
+                />
+              </div>
+              <div className="responsive-container-block img-cont">
+                <img className="img" src="/Assests/Gallery/recep book.jpg" />
+                <img className="img" src="/Assests/Gallery/LETTER HEAD.jpg" />
+                <img
+                  className="img"
+                  src="/Assests/Gallery/shape_cutting-removebg-preview.png"
+                />
+                    <img
+                  className="img img-big img-last"
+                  src="/Assests/Gallery/customercake.jpg"
+                />
+              </div>
+            </div>
+          </div>
         </div>
       </div>
+
       <Footer />
     </div>
   );
