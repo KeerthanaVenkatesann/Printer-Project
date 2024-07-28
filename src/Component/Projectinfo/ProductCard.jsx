8451fb577e27e32b674a0ad70e54456342fcbb69
Keerthana Venkatesan <135305964+KeerthanaVenkatesann@users.noreply.github.com>
2024-07-28 10:32:00 +0530

    whatsapp model

--- src/Component/Projectinfo/ProductCard.jsx
+++ src/Component/Projectinfo/ProductCard.jsx
@@ -1,22 +1,187 @@
+// import React from 'react';
+// import { FaWhatsapp } from 'react-icons/fa';
+// import './ProductCard.css';
+// import { PiWhatsappLogoDuotone } from "react-icons/pi";
 
-import React from 'react';
-import { FaWhatsapp } from 'react-icons/fa';
-import './ProductCard.css';
+// const ProductCard = ({ image, title, whatsappMessage }) => {
+//   const handleWhatsAppRedirect = (phoneNumber) => {
+//     const url = `https://wa.me/${phoneNumber}?text=${encodeURIComponent(whatsappMessage)}`;
+//     window.location.href = url;
+//   };
+//   return (
+//     <div><div className="product-card">
+//       <img src={image} alt={title} className="product-image" />
+
+//     </div> <div className="product-info text-center">
+
+//                 <a href="" target="_blank" rel="noopener noreferrer" className="whatsapp-button-two "   data-toggle="modal"
+//         data-target="#whatsappModal">
+//                   <FaWhatsapp className="whatsapp-icon fs-4" /> {title}
+//                 </a>
+//               </div>
+
+//               <div
+//         className="modal fade"
+//         id="whatsappModal"
+//         tabIndex="-1"
+//         aria-labelledby="whatsappModalLabel"
+//         aria-hidden="true"
+//       >
+//         <div className="modal-dialog">
+//           <div className="modal-content">
+//             <div className="modal-header">
+//               <h5 className="modal-title header-list" id="whatsappModalLabel">
+//               Choose Our Branches
+//               </h5>
+//               <button
+//                 type="button"
+//                 className="close"
+//                 data-dismiss="modal"
+//                 aria-label="Close"
+//               >
+//                 <span aria-hidden="true">&times;</span>
+//               </button>
+//             </div>
+//             <div className="modal-body ">
+//               <div className="mb-3">
+//                 <h5>Vadapalani</h5>
+
+//                 <button
+//                   className="btn btn-outline-danger"
+//                   onClick={() => handleWhatsAppRedirect("8681000087")}
+//                   data-dismiss="modal"
+//                 >
+//                   Message <PiWhatsappLogoDuotone />
+//                 </button>
+//               </div>
+//               <div>
+//                 <h5>Ashok Nagar</h5>
+
+//                 <button
+//                   className="btn btn-outline-danger"
+//                   onClick={() => handleWhatsAppRedirect("8681000086")}
+//                   data-dismiss="modal"
+//                 >
+//                   Message <PiWhatsappLogoDuotone />
+//                 </button>
+//               </div>
+//             </div>
+//             <div className="modal-footer">
+//               <button
+//                 type="button"
+//                 className="btn btn-dark"
+//                 data-dismiss="modal"
+//               >
+//                 Close
+//               </button>
+//             </div>
+//           </div>
+//         </div>
+//       </div>
+//               </div>
+//   );
+// };
+
+// export default ProductCard;
+
+import React from "react";
+import { FaWhatsapp } from "react-icons/fa";
+import "./ProductCard.css";
+import { PiWhatsappLogoDuotone } from "react-icons/pi";
 
 const ProductCard = ({ image, title, whatsappMessage }) => {
-  const whatsappLink = `https://wa.me/918681000087?text=${encodeURIComponent(whatsappMessage)}`;
+  const handleWhatsAppRedirect = (phoneNumber) => {
+    const url = `https://wa.me/${phoneNumber}?text=${encodeURIComponent(
+      whatsappMessage
+    )}`;
+    window.open(url, "_blank");
+  };
 
   return (
-    <div><div className="product-card">
-      <img src={image} alt={title} className="product-image" />
-     
-      
-    </div> <div className="product-info text-center">
+    <div>
+      <div className="product-card">
+        <img src={image} alt={title} className="product-image" />
+      </div>
+      <div className="product-info text-center">
+        <a
+          href="#"
+          target="_blank"
+          rel="noopener noreferrer"
+          className="whatsapp-button-two"
+          data-toggle="modal"
+          data-target="#whatsappModal"
+        >
+          <FaWhatsapp className="whatsapp-icon fs-4" /> {title}
+        </a>
+      </div>
+
+      <div
+        className="modal fade"
+        id="whatsappModal"
+        tabIndex="-1"
+        aria-labelledby="whatsappModalLabel"
+        aria-hidden="true"
+      >
+        <div className="modal-dialog">
+          <div className="modal-content">
+            <div className="modal-header">
+              <h5 className="modal-title header-list" id="whatsappModalLabel">
+                Choose Our Branches
+              </h5>
+              <button
+                type="button"
+                className="close"
+                data-dismiss="modal"
+                aria-label="Close"
+              >
+                <span aria-hidden="true">&times;</span>
+              </button>
+            </div>
+            <div className="modal-body">
+              <div className="mb-3">
+                <h5>Vadapalani</h5>
+                <button
+                  className="btn btn-outline-danger"
+                  onClick={() =>
+                    handleWhatsAppRedirect(
+                      "8681000087"
        
-                <a href={whatsappLink} target="_blank" rel="noopener noreferrer" className="whatsapp-button-two ">
-                  <FaWhatsapp className="whatsapp-icon fs-4" /> {title}
-                </a>
-              </div></div>
+                    )
+                  }
+                  data-dismiss="modal"
+                >
+                  Message <PiWhatsappLogoDuotone />
+                </button>
+              </div>
+              <div>
+                <h5>Ashok Nagar</h5>
+                <button
+                  className="btn btn-outline-danger"
+                  onClick={() =>
+                    handleWhatsAppRedirect(
+                      "8681000086"
+                      
+                    )
+                  }
+                  data-dismiss="modal"
+                >
+                  Message <PiWhatsappLogoDuotone />
+                </button>
+              </div>
+            </div>
+            <div className="modal-footer">
+              <button
+                type="button"
+                className="btn btn-dark"
+                data-dismiss="modal"
+              >
+                Close
+              </button>
+            </div>
+          </div>
+        </div>
+      </div>
+    </div>
   );
 };
 
--- src/Component/Whatsapp/Whatsapp.jsx
+++ src/Component/Whatsapp/Whatsapp.jsx
@@ -19,14 +19,98 @@
 //   );
 // }
 
+// import React from "react";
+// import "./Whatsapp.css";
+// import { PiWhatsappLogoDuotone } from "react-icons/pi";
+
+// export default function Whatsapp() {
+//   const handleWhatsAppRedirect = (phoneNumber) => {
+//     const url = `https://wa.me/${phoneNumber}`;
+//     window.location.href = url;
+//   };
+
+//   return (
+//     <div>
+//       <button
+//         className="btn btn-outline-black get-button-two content-font"
+//         data-toggle="modal"
+//         data-target="#whatsappModal"
+//       >
+//         Direct WhatsApp <PiWhatsappLogoDuotone />
+//       </button>
+
+//       <div
+//         className="modal fade"
+//         id="whatsappModal"
+//         tabIndex="-1"
+//         aria-labelledby="whatsappModalLabel"
+//         aria-hidden="true"
+//       >
+//         <div className="modal-dialog">
+//           <div className="modal-content">
+//             <div className="modal-header">
+//               <h5 className="modal-title header-list" id="whatsappModalLabel">
+//               Choose Our Branches
+//               </h5>
+//               <button
+//                 type="button"
+//                 className="close"
+//                 data-dismiss="modal"
+//                 aria-label="Close"
+//               >
+//                 <span aria-hidden="true">&times;</span>
+//               </button>
+//             </div>
+//             <div className="modal-body ">
+//               <div className="mb-3">
+//                 <h5>Vadapalani</h5>
+
+//                 <button
+//                   className="btn btn-outline-danger"
+//                   onClick={() => handleWhatsAppRedirect("8681000087")}
+//                   data-dismiss="modal"
+//                 >
+//                   Message <PiWhatsappLogoDuotone />
+//                 </button>
+//               </div>
+//               <div>
+//                 <h5>Ashok Nagar</h5>
+
+//                 <button
+//                   className="btn btn-outline-danger"
+//                   onClick={() => handleWhatsAppRedirect("8681000086")}
+//                   data-dismiss="modal"
+//                 >
+//                   Message <PiWhatsappLogoDuotone />
+//                 </button>
+//               </div>
+//             </div>
+//             <div className="modal-footer">
+//               <button
+//                 type="button"
+//                 className="btn btn-dark"
+//                 data-dismiss="modal"
+//               >
+//                 Close
+//               </button>
+//             </div>
+//           </div>
+//         </div>
+//       </div>
+//     </div>
+//   );
+// }
 import React from "react";
 import "./Whatsapp.css";
 import { PiWhatsappLogoDuotone } from "react-icons/pi";
 
 export default function Whatsapp() {
   const handleWhatsAppRedirect = (phoneNumber) => {
-    const url = `https://wa.me/${phoneNumber}`;
-    window.location.href = url;
+    const message = encodeURIComponent(
+      "Hello, Allwin Digital! I would like to know about your products and service."
+    );
+    const url = `https://wa.me/${phoneNumber}?text=${message}`;
+    window.open(url, "_blank");
   };
 
   return (
@@ -39,41 +123,54 @@ export default function Whatsapp() {
         Direct WhatsApp <PiWhatsappLogoDuotone />
       </button>
 
-      <div className="modal fade" id="whatsappModal" tabIndex="-1" aria-labelledby="whatsappModalLabel" aria-hidden="true">
+      <div
+        className="modal fade"
+        id="whatsappModal"
+        tabIndex="-1"
+        aria-labelledby="whatsappModalLabel"
+        aria-hidden="true"
+      >
         <div className="modal-dialog">
           <div className="modal-content">
             <div className="modal-header">
-              <h5 className="modal-title header-list" id="whatsappModalLabel">Select Our Branch</h5>
-              <button type="button" className="close" data-dismiss="modal" aria-label="Close">
+              <h5 className="modal-title header-list" id="whatsappModalLabel">
+                Choose Our Branches
+              </h5>
+              <button
+                type="button"
+                className="close"
+                data-dismiss="modal"
+                aria-label="Close"
+              >
                 <span aria-hidden="true">&times;</span>
               </button>
             </div>
             <div className="modal-body">
               <div className="mb-3">
-                <h6>Vadapalani</h6>
-               
+                <h5>Vadapalani</h5>
                 <button
                   className="btn btn-outline-danger"
                   onClick={() => handleWhatsAppRedirect("8681000087")}
-                  data-dismiss="modal"
                 >
                   Message <PiWhatsappLogoDuotone />
                 </button>
               </div>
               <div>
-                <h6>Ashok Nagar</h6>
-               
+                <h5>Ashok Nagar</h5>
                 <button
                   className="btn btn-outline-danger"
                   onClick={() => handleWhatsAppRedirect("8681000086")}
-                  data-dismiss="modal"
                 >
                   Message <PiWhatsappLogoDuotone />
                 </button>
               </div>
             </div>
             <div className="modal-footer">
-              <button type="button" className="btn btn-dark" data-dismiss="modal">
+              <button
+                type="button"
+                className="btn btn-dark"
+                data-dismiss="modal"
+              >
                 Close
               </button>
             </div>
